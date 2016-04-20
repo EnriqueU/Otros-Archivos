747191cdfc4f61351254f3dc59ba8da8aeffe9e4
C-Lara <claraa@uni.edu.pe>
2016-04-19 23:30:57 -0500

    Ejemplo en Python

---
+++ Linea_de_comandos_Linux/Ejemplo.py
@@ -0,0 +1,9 @@
+class Estudiante:
+  def __init__ (self, nombre, edad, genero):
+    self.nombre   = name
+    self.edad    = edad
+    self.genero = genero
+
+Sue = Student("Mili LLam", 28, "f")
+print Sue
+print Sue.edad
